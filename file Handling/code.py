3d8ac12d269f3a360d3376c52f1f9a60453a6f5b
UM1985 <56utkarshpatel@gmail.com>
2025-03-04 18:08:26 +0530

    file handling

---
+++ file Handling/code.py
@@ -0,0 +1,9 @@
+'''
+File handling in Python refers to the process of working with files on your computer. This includes creating, reading, updating, and deleting files. Python provides built-in functions and methods to perform these operations.
+
+
+
+
+
+
+'''
